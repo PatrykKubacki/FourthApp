f1e7a54b1df3db3250203259911614812e783147
patryk.kubacki@outlook.com <patryk.kubacki@outlook.com>
2017-05-10 07:37:01 +0200

    Update

---
+++ FourthApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Web.Mvc;
+using FourthApp.Models;
+
+namespace FourthApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if(!Request.IsAjaxRequest())
+                return Content($"Imie: {user.FirstName}, Nazwisko: {user.LastName}, Wiek: {user.Age}");
+
+            return Json(user);
+
+        }
+
+        public string GetTime()
+        {
+            Thread.Sleep(2000);
+            return DateTime.Now.ToLongTimeString();
+        }
+    }
+}
